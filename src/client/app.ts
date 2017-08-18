3db1bb2d25c08bda675de1925dd7a902116d722b
asakusuma <asa.kusuma@gmail.com>
2017-08-18 10:17:02 -0700

    Render different apps from server based on config

--- src/client/app.ts
+++ src/client/app.ts
@@ -1,4 +1,16 @@
-import { hrefToName } from './../shared/shared';
+import { hrefToName, isAdjunct } from './../shared/shared';
+
+declare var IS_ADJUNCT: boolean;
+const portal = document.getElementById('transmogrifier-portal') as HTMLIFrameElement;
+(window as any).portal = portal;
+(window as any).IS_ACTIVE = true;
+function isChild(): boolean {
+  return window.parent !== window;
+}
+
+function shouldTransmogrify(path: string): boolean {
+  return !!(Number(isAdjunct(path)) ^ Number(IS_ADJUNCT));
+}
 
 document.addEventListener('click', (e) => {
   const el: Element = e.target as Element;
@@ -6,18 +18,66 @@ document.addEventListener('click', (e) => {
     const href = el.getAttribute('href');
     if (href) {
       e.preventDefault();
-      window.history.pushState(null, null, href);
+      updateUrl(href)
       navigateTo(href);
     }
   }
 });
 
 window.addEventListener('popstate', (e: PopStateEvent) => {
-  navigateTo(window.location.pathname);
+  const href = window.location.pathname;
+  if (!isChild() && !isParentActive()) {
+    exitPortal(href);
+  } else {
+    navigateTo(href);
+  }
 });
 
 function navigateTo(href: string) {
   console.log('Navigate', href);
+  if (shouldTransmogrify(href)) {
+    if (isChild()) {
+      (window.parent as any).exitPortal(href);
+    } else {
+      enterPortal(href);
+    }
+  } else {
+    routeTo(href);
+  }
+}
+
+function routeTo(href: string) {
+  if (isChild()) {
+    (window.parent as any).updateUrl(href);
+  }
   const appEl = document.getElementById('app');
   appEl.textContent = hrefToName(href);
 }
+
+function enterPortal(href: string) {
+  console.log('enter');
+  (window as any).IS_ACTIVE = false;
+  (portal.contentWindow as any).routeTo(href);
+  document.getElementById('transmogrifier-portal').style.display = 'block';
+}
+
+function exitPortal(href: string) {
+  (window as any).IS_ACTIVE = true;
+  updateUrl(href);
+  routeTo(href);
+  document.getElementById('transmogrifier-portal').style.display = 'none';
+}
+
+function updateUrl(href: string) {
+  window.history.pushState(null, null, href);
+}
+
+function isParentActive(): boolean {
+  return isChild() ? (window.parent as any).isParentActive() : (window as any).IS_ACTIVE;
+}
+
+(window as any).routeTo = routeTo;
+(window as any).exitPortal = exitPortal;
+(window as any).navigateTo = navigateTo;
+(window as any).updateUrl = updateUrl;
+(window as any).isParentActive = isParentActive;
--- src/server.ts
+++ src/server.ts
@@ -5,45 +5,62 @@ const app = express();
 app.use('/client', express.static('./dist/client'));
 app.use('/client/almond.js', express.static('./lib/almond.js'));
 
-function pageHandler(name: string, isAdjunct: boolean = false) {
-  const isAdjunctString = isAdjunct ? 'true' : 'false';
-  const color = isAdjunct ? 'orange' : 'blue';
-  const template =
-  `<html>
-    <head>
-      <style>
-        html, body {
-          color: ${color};
-        }
-      </style>
-      <script>
-        window.IS_ADJUNCT = "${isAdjunctString}";
-      </script>
-      <script type="text/javascript" src="client/almond.js"></script>
-      <script type="text/javascript" src="client/index.js"></script>
-      <script>
-        require('client/app');
-      </script>
-    </head>
-    <body>
-      <ul>
-        <li><a href="/">home</a></li>
-        <li><a href="/foo">foo</a></li>
-        <li><a href="/bar">bar</a></li>
-        <li><a href="/baz">baz</a></li>
-      </ul>
-      <div id="app">${name}</div>
-    </body>
-  </html>`;
-  return function serveApp(req: express.Request, res: express.Response) {
+function renderPage(name: string, color: string, isAdjunctString: string, childAppName?: string) {
+  const iframe = childAppName ? `<iframe id="transmogrifier-portal" frameborder="0" seamless="true" style="display:none;" scrolling="no" src="http://localhost:3000/?appName=${childAppName}"></iframe>` : '';
+  return `<html>
+      <head>
+        <style>
+          html, body {
+            color: ${color};
+            background-color: #FFF;
+          }
+          #transmogrifier-portal {
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+          }
+        </style>
+        <script>
+          window.IS_ADJUNCT = ${isAdjunctString};
+        </script>
+        <script type="text/javascript" src="client/almond.js"></script>
+      </head>
+      <body>
+        <ul>
+          <li><a href="/">home</a></li>
+          <li><a href="/foo">foo</a></li>
+          <li><a href="/bar">bar</a></li>
+          <li><a href="/baz">baz</a></li>
+        </ul>
+        <div id="app">${name}</div>
+        ${iframe}
+        <script type="text/javascript" src="client/index.js"></script>
+        <script>
+          require('client/app');
+        </script>
+      </body>
+    </html>`;
+}
+
+function pageHandler(name: string, isAdj: boolean = false) {
+  return function serveApp(req: express.Request, res: express.Response, appName?: AppName) {
+    const isAdjunct = isAdj || (appName && appName === 'adjunct');
+    const isAdjunctString = isAdjunct  ? 'true' : 'false';
+    const color = isAdjunct ? 'orange' : 'blue';
+    const childAppName: AppName = appName ? null : (!isAdjunct ? 'adjunct' : 'incumbent');
+
     res.set('Content-Type', 'text/html');
-    res.send(template);
+    res.send(renderPage(name, color, isAdjunctString, childAppName));
   }
 }
 
 interface Handler {
   match: (s: string) => boolean;
-  callback: (req: express.Request, res: express.Response) => void;
+  callback: (req: express.Request, res: express.Response, appName?: AppName) => void;
 }
 
 function generateHandlers(routes: string[]): Handler[] {
@@ -61,18 +78,34 @@ function generateHandlers(routes: string[]): Handler[] {
 
 const handlers = generateHandlers(['home', 'foo', 'bar', 'baz']);
 
+app.get('/sw.js', function(req, res) {
+  res.status(404).send();
+});
+
+type AppName = 'adjunct' | 'incumbent';
+
+function sanitizeAppNameDirective(directive: string): AppName {
+  if (typeof directive !== 'string' || !directive) {
+    return null;
+  }
+  if (directive === 'adjunct') {
+    return 'adjunct';
+  }
+  return 'incumbent';
+}
+
 app.get('*', function(req, res) {
   let responded = false;
+  const forceAppName = req.query.appName;
   for (let i = 0; i < handlers.length; i++) {
     if (handlers[i].match(req.path)) {
-      console.log('Matched', req.path, );
-      handlers[i].callback(req, res);
+      handlers[i].callback(req, res, sanitizeAppNameDirective(forceAppName));
       responded = true;
     }
   }
 
   if (!responded) {
-    res.status(404);
+    res.status(404).end();
   }
 });
 
